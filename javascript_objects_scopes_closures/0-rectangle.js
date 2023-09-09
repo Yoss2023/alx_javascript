d98b522db0d8ecf044a3b98570472b81ce13bedb
youssra <youssraanene20@gmail.com>
2023-09-09 08:18:41 -0700

    Rectangle #0

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
-const Rectangle = require('./0-rectangle');
 
-const r1 = new Rectangle();
-console.log(r1);
-console.log(r1.constructor);
+class Rectangle {
+  constructor() {
+    // This is an empty class constructor
+  }
+}
+
+module.exports = Rectangle;
+
 
