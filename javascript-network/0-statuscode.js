6e14d20ab34e48142ae18c75acf4650a108a18a5
youssra <youssraanene20@gmail.com>
2023-09-09 08:32:20 -0700

    Status code

---
+++ javascript-network/0-statuscode.js
@@ -0,0 +1,18 @@
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: node 0-statuscode.js <URL>');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+
+request(url, (error, response) => {
+  if (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+  
+  console.log(`code: ${response.statusCode}`);
+});
+
